1073c905fa68cd419f1cea5ccb606818125eeb29
Sasi Kumar M <sasikumarmurugan02@gmail.com>
2023-10-23 10:15:41 +0530

    23-10-2023

--- rest-api/index.js
+++ rest-api/index.js
@@ -3,16 +3,19 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-
+const dotenv = require(dotenv);
+dotenv.config();
 app.use(bodyParser.json());
 app.use(cors());
+const MONGO_URI = process.env.MONGO_URI
+const port = process.env.port || 8000;
 mongoose
-  .connect('mongodb+srv://sasikumar:sasikumar@admin.xuv4ysl.mongodb.net/bookstore?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.log(err));
 const bookRoute = require('./node-backend/routes/book.routes'); 
 app.use('/api', bookRoute); 
 
-app.listen(8000, () => {
+app.listen(port, () => {
   console.log(`Server is running on port 8000`);
 });
